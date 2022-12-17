e372391c7a2a547ccf5be97e6ebc92f5cf69ff19
tarunmandhan <mandhantarun@gmail.com>
2022-12-18 03:36:50 +0530

    add code from demoapp27

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import ReactDOM from "react-dom";
+
 
 const App = () => {
-  return;
-};
+
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+import TodoList from "./TodoList";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<TodoList />, document.getElementById("root"));
